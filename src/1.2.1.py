88ed8c320e687b7bf3af4e27e4b5d0b487149fc0
et116-kadyshevsm <kadyshev74@gmail.com>
2026-04-17 09:13:31 +0500

    Диаграммы распределения

---
+++ src/1.2.1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('mental_health_risk_dataset.csv')
+
+numeric_cols_info = [
+    ('age', 'Возраст'),
+    ('sleep_hours', 'Часы сна'),
+    ('physical_activity_hours_per_week', 'Физ. активность (ч/нед)'),
+    ('screen_time_hours_per_day', 'Экранное время (ч/день)'),
+    ('social_support_score', 'Социальная поддержка (балл)'),
+    ('work_stress_level', 'Рабочий стресс (уровень)'),
+    ('academic_pressure_level', 'Академическое давление (уровень)'),
+    ('job_satisfaction_score', 'Удовлетворённость работой (балл)'),
+    ('financial_stress_level', 'Финансовый стресс (уровень)'),
+    ('working_hours_per_week', 'Рабочие часы в неделю'),
+    ('anxiety_score', 'Тревожность (балл)'),
+    ('depression_score', 'Депрессия (балл)'),
+    ('stress_level', 'Стресс (уровень)'),
+    ('mood_swings_frequency', 'Частота смены настроения'),
+    ('concentration_difficulty_level', 'Сложность концентрации')
+]
+
+n = len(numeric_cols_info)
+ncols = 3
+nrows = (n + ncols - 1) // ncols
+
+fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(18, 5 * nrows))
+axes = axes.flatten()
+
+for i, (col, ru_name) in enumerate(numeric_cols_info):
+    axes[i].hist(df[col], bins=30, color='skyblue', edgecolor='black', alpha=0.7)
+    axes[i].set_title(f'{ru_name}')
+    axes[i].set_xlabel(ru_name)
+    axes[i].set_ylabel('Частота')
+
+    for j in range(i + 1, len(axes)):
+        axes[j].axis('off')
+
+plt.tight_layout()
+plt.savefig('Распределения гистограм.png', dpi=150, bbox_inches='tight')
+print("Гистограммы распределений сохранены в файл 'Распределения гистограм.png'")
